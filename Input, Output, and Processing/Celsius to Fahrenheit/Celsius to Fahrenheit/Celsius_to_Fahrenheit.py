3dbf07bd79a7d653e3cd6c3069ca68a6f8fcfc28
Ryan Yun <yunr@oregonstate.edu>
2018-10-16 01:28:06 +0900

    Cels to Fahr converter

---
+++ Input, Output, and Processing/Celsius to Fahrenheit/Celsius to Fahrenheit/Celsius_to_Fahrenheit.py
@@ -0,0 +1,7 @@
+#Function that converts CELSIUS to FAHRENHEIT
+
+celsius = int(input("Enter the Celsius temperature you wish to convert to Fahrenheit: "));
+
+fahrenheit = ((9/5) * celsius) + 32;
+
+print("Conversion from",celsius,"celsius to fahrenheit:", fahrenheit);
